65b520ad2f3d18c9839b7518c233a5f6b7e438ed
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-01 11:29:37 +0530

    not complete yet

---
+++ script.js
@@ -0,0 +1,7 @@
+const colorPicker = document.getElementById("colorPicker");
+const canvasColor = document.getElementById("canvasColor");
+const canvas = document.getElementById("myCanvas")
+const clearButton = document.getElementById("clearButton")
+const saveButton = document.getElementById("saveButton")
+const retriveButton = document.getElementById("retriveButton")
+const fontPicker = document.getElementById('fontPicker')
